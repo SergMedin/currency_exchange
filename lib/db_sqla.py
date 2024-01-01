f47f06c4ed47ea51d56f68e779e0691b2d9c7a53
Andrey Mescheryakov <fant.amp@gmail.com>
2024-01-01 20:32:36 +0000

    Hide module's internal stuff

--- lib/db_sqla.py
+++ lib/db_sqla.py
@@ -8,11 +8,52 @@ from .db import Db
 from .data import Order, OrderType, User
 
 
-class Base(DeclarativeBase):
+class SqlDb(Db):
+
+    def __init__(self):
+        self._eng = create_engine("sqlite://", echo=False)
+        _Base.metadata.create_all(self._eng)
+
+    def get_order(self, id: int) -> Order:
+        with Session(self._eng) as session:
+            o = session.get(_DbOrder, id)
+            d = _ORDERS_TABLE.mk_dict(o, False)
+        return Order(**d)
+
+    def store_order(self, o: Order) -> Order:
+        with Session(self._eng) as session:
+            d = _ORDERS_TABLE.mk_dict(o, True)
+            o_db = _DbOrder(**d)
+            session.add(o_db)
+            session.commit()
+            id = o_db.id
+        return self.get_order(id)
+
+    def update_order(self, o: Order):
+        with Session(self._eng) as session:
+            dbo = session.get(_DbOrder, o._id)
+            for field_name, value in _ORDERS_TABLE.mk_dict(o, True).items():
+                setattr(dbo, field_name, value)  # updates only changed fields
+            session.commit()
+
+    def remove_order(self, id: int):
+        with Session(self._eng) as session:
+            session.delete(session.get(_DbOrder, id))
+            session.commit()
+
+    def iterate_orders(self, callback: Callable[[Order], None]):
+        with Session(self._eng) as session:
+            for dbo in session.scalars(select(_DbOrder)):
+                d = _ORDERS_TABLE.mk_dict(dbo, False)
+                o = Order(**d)
+                callback(o)
+
+
+class _Base(DeclarativeBase):
     pass
 
 
-class DbOrder(Base):
+class _DbOrder(_Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -46,25 +87,25 @@ class DbOrder(Base):
     min_op_threshold_cents: Mapped[int] = mapped_column(nullable=False)
 
 
-ValueConvertFunctionType = Callable[[Any,], Any]
+_ValueConvertFunctionType = Callable[[Any,], Any]
 
 
 @dataclasses.dataclass
-class Field:
+class _Field:
     do_name: str
     db_name: str
-    do2db: ValueConvertFunctionType = None
-    db2do: ValueConvertFunctionType = None
+    do2db: _ValueConvertFunctionType = None
+    db2do: _ValueConvertFunctionType = None
     is_id: bool = False
 
 
 @dataclasses.dataclass
-class Table:
+class _Table:
     name: str  # FIXME: never used?
     do_class: Callable  # FIXME: never used?
-    fields: list[Field]
+    fields: list[_Field]
 
-    def get_id_field(self) -> Field:
+    def get_id_field(self) -> _Field:
         return [f for f in self.fields if f.is_id][0]
 
     def mk_dict(self, obj: Any, is_do: bool) -> dict:
@@ -79,61 +120,20 @@ class Table:
         return d
 
 
-ORDERS_TABLE = Table("orders", Order, [
-    Field("_id", "id", None, None, True),
-    Field("type", "type", lambda x: int(x), lambda x: OrderType(x)),
-    Field("lifetime", "lifetime"),
-    Field("creation_time", "creation_time"),
-    Field("user", "user_id", lambda x: x.id, lambda x: User(x)),
-    Field("price", "price_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
-    Field("amount_initial", "amount_initial_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
-    Field("amount_left", "amount_left_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
-    Field("min_op_threshold", "min_op_threshold_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
+_ORDERS_TABLE = _Table("orders", Order, [
+    _Field("_id", "id", None, None, True),
+    _Field("type", "type", lambda x: int(x), lambda x: OrderType(x)),
+    _Field("lifetime", "lifetime"),
+    _Field("creation_time", "creation_time"),
+    _Field("user", "user_id", lambda x: x.id, lambda x: User(x)),
+    _Field("price", "price_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
+    _Field("amount_initial", "amount_initial_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
+    _Field("amount_left", "amount_left_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
+    _Field("min_op_threshold", "min_op_threshold_cents", lambda x: int(x*100), lambda x: x/Decimal(100.0)),
 ])
 
 
-class SqlDb(Db):
-
-    def __init__(self):
-        self._eng = create_engine("sqlite://", echo=False)
-        Base.metadata.create_all(self._eng)
-
-    def get_order(self, id: int) -> Order:
-        with Session(self._eng) as session:
-            o = session.get(DbOrder, id)
-            d = ORDERS_TABLE.mk_dict(o, False)
-        return Order(**d)
-
-    def store_order(self, o: Order) -> Order:
-        with Session(self._eng) as session:
-            d = ORDERS_TABLE.mk_dict(o, True)
-            o_db = DbOrder(**d)
-            session.add(o_db)
-            session.commit()
-            id = o_db.id
-        return self.get_order(id)
-
-    def update_order(self, o: Order):
-        with Session(self._eng) as session:
-            dbo = session.get(DbOrder, o._id)
-            for field_name, value in ORDERS_TABLE.mk_dict(o, True).items():
-                setattr(dbo, field_name, value)  # updates only changed fields
-            session.commit()
-
-    def remove_order(self, id: int):
-        with Session(self._eng) as session:
-            session.delete(session.get(DbOrder, id))
-            session.commit()
-
-    def iterate_orders(self, callback: Callable[[Order], None]):
-        with Session(self._eng) as session:
-            for dbo in session.scalars(select(DbOrder)):
-                d = ORDERS_TABLE.mk_dict(dbo, False)
-                o = Order(**d)
-                callback(o)
-
-
-class T(unittest.TestCase):
+class _T(unittest.TestCase):
     def setUp(self):
         self.db = SqlDb()
 
