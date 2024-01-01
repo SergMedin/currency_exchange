d2344e645e0c98b6e9fb30949175463cf8acd896
SergMedin <47296603+SergMedin@users.noreply.github.com>
2024-01-01 22:57:41 +0100

    Log sqlalchemy SQL statements to the standard logger with DEBUG level

--- lib/logger.py
+++ lib/logger.py
@@ -6,11 +6,15 @@ def get_logger(name):
     logger.setLevel(logging.INFO)
 
     handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
+    handler.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s:\n %(message)s')
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
+
+    sqlalchemy_logger = logging.getLogger('sqlalchemy')
+    sqlalchemy_logger.setLevel(logging.DEBUG)
+    sqlalchemy_logger.addHandler(handler)
 
     return logger
