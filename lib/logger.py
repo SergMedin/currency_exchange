d4ac371ac09b97003a8152aca637d51029066c0d
Sergei Baryshevskii <black3knight@gmail.com>
2024-01-02 15:53:44 +0000

    Add ovverride for logging

--- lib/logger.py
+++ lib/logger.py
@@ -1,19 +1,27 @@
 import logging
+import os
+
+from dotenv import load_dotenv
 
 
 def get_logger(name):
+    load_dotenv()
+    LOG_LVL = os.getenv("LOG_LVL", "INFO")
+    CONSOLE_LOG_LVL = os.getenv("CONSOLE_LOG_LVL", "ERROR")
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(LOG_LVL)
 
     handler = logging.StreamHandler()
-    handler.setLevel(logging.ERROR)
+    handler.setLevel(CONSOLE_LOG_LVL)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s:\n %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s:\n %(message)s"
+    )
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
 
-    sqlalchemy_logger = logging.getLogger('sqlalchemy')
+    sqlalchemy_logger = logging.getLogger("sqlalchemy")  # Not in usage
     sqlalchemy_logger.setLevel(logging.DEBUG)
     sqlalchemy_logger.addHandler(handler)
 
