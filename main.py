f2fc598f6769c50e4f03c2527e2d0aa978951f5d
Andrey Mescheryakov <fant.amp@gmail.com>
2024-01-05 22:55:25 +0000

    Get db path from env

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+import os
 from lib.tg import TelegramReal
 from lib.tg_app import TgApp
 from lib.db_sqla import SqlDb
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    conn_str = os.getenv("EXCH_DB_CONN_STRING", "sqlite:///exchange_database.sqlite")
     telegram = TelegramReal()
-    app = TgApp(db=SqlDb("sqlite:///exchange_database.sqlite"), tg=telegram)
+    app = TgApp(db=SqlDb(conn_str), tg=telegram)
     telegram.run_forever()
