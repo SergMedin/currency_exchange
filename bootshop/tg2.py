3a845209bec935e9c25cc153bdfa4865c4f89798
Andrey Meshcheryakov <fant.amp@gmail.com>
2024-01-11 19:27:00 +0000

    Recieve pressed button events

--- bootshop/tg2.py
+++ bootshop/tg2.py
@@ -1,16 +1,17 @@
+import logging
 import asyncio
-from lib.tg import Tg, TgIncomingMsg, TgOutgoingMsg
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, ContextTypes, MessageHandler, filters, CallbackQueryHandler
+from lib.tg import Tg, TgIncomingMsg, TgOutgoingMsg
 
 
 class TgReal2(Tg):
     def __init__(self, token: str):
         self.application = Application.builder().token(token).build()
         self.application.add_handler(
-            MessageHandler(filters.TEXT, self._default_handler)
+            MessageHandler(filters.TEXT, self._default_handler),
         )
+        self.application.add_handler(CallbackQueryHandler(self._callback_query_handler))
 
     def run_forever(self):
         self.application.run_polling(allowed_updates=Update.ALL_TYPES)
@@ -27,6 +28,14 @@ class TgReal2(Tg):
             self.on_message(message)
         except ValueError as e:
             await update.message.reply_text(f"Error: {str(e)}")
+
+    async def _callback_query_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        logging.info(f"got callback query. Update: {update}")
+        query = update.callback_query
+        # CallbackQueries need to be answered, even if no notification to the user is needed
+        # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+        await query.answer()
+        await query.edit_message_text(text=f"Selected option: {query.data}")
 
     def send_message(self, m: TgOutgoingMsg, parse_mode=None):
         keyboard = [
