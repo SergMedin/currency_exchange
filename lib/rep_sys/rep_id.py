833270c93a38582a03080e6c0445c8d6c079a0e5
Andrey Meshcheryakov <fant.amp@gmail.com>
2024-03-02 23:11:48 +0000

    Add email misuse protection

--- lib/rep_sys/rep_id.py
+++ lib/rep_sys/rep_id.py
@@ -1,5 +1,20 @@
 from dataclasses import dataclass
+from typing import Optional
+from hashlib import sha256
+
 
 @dataclass
 class RepSysUserId:
-    telegram_user_id: int
+    telegram_user_id: Optional[int] = None
+    email_hash: Optional[str] = None
+
+    @classmethod
+    def hash_email(cls, email: str) -> str:
+        return sha256((email + EMAIL_SALT).encode()).hexdigest()
+
+    @classmethod
+    def from_email(cls, email: str) -> "RepSysUserId":
+        return cls(email_hash=cls.hash_email(email))
+
+
+EMAIL_SALT = "oETx$&xScuwcsmVh"
